b8f6222023bdb1fc62b3f2b8cf797cfd03cc8952
kasaiS-2-S-2 <gofrvn@gmail.com>
2021-07-15 01:19:33 +0900

    Initial commit

---
+++ app/src/main/java/com/kasai/speed_whether/ui/main/MainFragment.kt
@@ -0,0 +1,26 @@
+package com.kasai.speed_whether.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.kasai.speed_whether.R
+
+
+
+class MainFragment : Fragment() {
+
+    private val viewModel: MainViewModel by activityViewModels()
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+}
