58eb2f112a79a72279079d7a09bf00ff6ac6059a
kasaiS-2-S-2 <gofrvn@gmail.com>
2021-08-03 23:34:06 +0900

    enabled to use the current place information

--- app/src/main/java/com/kasai/speed_whether/viewModel/WeatherInfoViewModel.kt
+++ app/src/main/java/com/kasai/speed_whether/viewModel/WeatherInfoViewModel.kt
@@ -40,4 +40,40 @@ class WeatherInfoViewModel : ViewModel() {
             }
         }
     }
+
+    private fun A() {
+// Use fields to define the data types to return.
+        val placeFields: List<Place.Field> = listOf(Place.Field.NAME)
+
+// Use the builder to create a FindCurrentPlaceRequest.
+        val request: FindCurrentPlaceRequest = FindCurrentPlaceRequest.newInstance(placeFields)
+
+// Call findCurrentPlace and handle the response (first check that the user has granted permission).
+        if (ContextCompat.checkSelfPermission(this, permission.ACCESS_FINE_LOCATION) ==
+            PackageManager.PERMISSION_GRANTED) {
+
+            val placeResponse = placesClient.findCurrentPlace(request)
+            placeResponse.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val response = task.result
+                    for (placeLikelihood: PlaceLikelihood in response?.placeLikelihoods ?: emptyList()) {
+                        Log.i(
+                            TAG,
+                            "Place '${placeLikelihood.place.name}' has likelihood: ${placeLikelihood.likelihood}"
+                        )
+                    }
+                } else {
+                    val exception = task.exception
+                    if (exception is ApiException) {
+                        Log.e(TAG, "Place not found: ${exception.statusCode}")
+                    }
+                }
+            }
+        } else {
+            // A local method to request required permissions;
+            // See https://developer.android.com/training/permissions/requesting
+            getLocationPermission()
+        }
+
+    }
 }
